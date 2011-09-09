938b5e89ac0500ccf76da0eb5d83e48bd2ba28c3
Nguyen Hoang Duy <duy.nghoang@gmail.com>
2011-09-10 01:08:32 +0800

    first commit

---
+++ CEGMarket/CEGMarketSystem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Member_Class;
+using Transaction_Class;
+using Product_Class;
+
+namespace CEGMarketSystem_Class
+{
+    class CEGMarketSystem
+    {
+    }
+}
